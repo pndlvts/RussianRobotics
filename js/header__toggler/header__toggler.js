c6d3b002fd5d91a4f7305bb0ba01fd052eada722
Pandelov Timofey <pndlvts@gmail.com>
2019-11-27 00:13:14 +0300

    Навбар под 320px готов

--- js/header__toggler/header__toggler.js
+++ js/header__toggler/header__toggler.js
@@ -6,6 +6,7 @@ box.classList.toggle("header__menu_show");
 document.querySelector("#for-person").addEventListener("click", (e) => {
 var box = document.querySelectorAll(".header__for-person-list-item");
 box.forEach(box => { box.classList.toggle("header__for-person-list-item_show"); })
+document.querySelector("#for-person-dropdown").classList.toggle("header__for-person_active");
 })
 
 
