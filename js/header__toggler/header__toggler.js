38098dc1e4daf7535dc90d0000c9e4394cf050a5
Pandelov Timofey <pndlvts@gmail.com>
2019-11-26 23:52:04 +0300

    Навбар под 320px готов

--- js/header__toggler/header__toggler.js
+++ js/header__toggler/header__toggler.js
@@ -1,4 +1,11 @@
 document.querySelector("#icon-menu").addEventListener("click", (e) => {
 var box = document.querySelector(".header__menu");
 box.classList.toggle("header__menu_show");
+});
+
+document.querySelector("#for-person").addEventListener("click", (e) => {
+var box = document.querySelectorAll(".header__for-person-list-item");
+box.forEach(box => { box.classList.toggle("header__for-person-list-item_show"); })
 })
+
+
