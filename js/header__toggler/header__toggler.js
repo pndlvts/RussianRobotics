08b9ba59d469a76478dc36c2b6997228d2872c5a
Pandelov Timofey <pndlvts@gmail.com>
2019-11-29 21:08:20 +0300

    готовый навбар для смартфонов и планшетов

--- js/header__toggler/header__toggler.js
+++ js/header__toggler/header__toggler.js
@@ -1,5 +1,6 @@
+//гамбергер
 document.querySelector("#icon-menu").addEventListener("click", (e) => {
-    if (document.body.clientWidth <= 768) {
+    if (document.body.clientWidth <= 767) {
         var box = document.querySelector("ul.header__menu");
         box.classList.toggle("header__menu_show");
     } else {
@@ -7,13 +8,14 @@ document.querySelector("#icon-menu").addEventListener("click", (e) => {
         box.classList.toggle("header__menu_show");
     }
 });
-
+//для частных лиц (на планшеты)
 document.querySelector("#for-person-tablet").addEventListener("click", (e) => {
     var box1 = document.querySelector(".header__for-person-block");
     box1.classList.toggle("header__for-person-block_show");
     document.querySelector("#for-person-tablet-link").classList.toggle("header__for-person_active");
 
 })
+//для частных лиц (на телефоны)
 document.querySelector("#for-person").addEventListener("click", (e) => {
     var box = document.querySelectorAll(".header__for-person-list-item");
     box.forEach(box => {
@@ -21,11 +23,17 @@ document.querySelector("#for-person").addEventListener("click", (e) => {
     })
     document.querySelector("#for-person-dropdown").classList.toggle("header__for-person_active");
 })
-
+//для юр лиц (на телефоны)
 document.querySelector("#for-corp").addEventListener("click", (e) => {
     var box = document.querySelectorAll(".header__for-corp-list-item");
     box.forEach(box => {
         box.classList.toggle("header__for-corp-list-item_show");
     })
     document.querySelector("#for-corp-dropdown").classList.toggle("header__for-corp_active");
+})
+//о банке (на планшеты)
+document.querySelector("#header__about-bank_tablet").addEventListener("click", (e) => {
+    var box = document.querySelector("ul.header__about-bank-list");
+    box.classList.toggle("header__about-bank-list_show");
+    document.querySelector(".header__about-bank-drop").classList.toggle("header__about-bank-drop_active");
 })
