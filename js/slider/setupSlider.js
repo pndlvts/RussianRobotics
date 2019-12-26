22c04938596b9bf85802fe8de5025f69d0b5fb13
Pandelov Timofey <pndlvts@gmail.com>
2019-12-27 01:17:28 +0300

    начат второй слайдер, осталось оформить слайды

--- js/slider/setupSlider.js
+++ js/slider/setupSlider.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('.owl-carousel-1').owlCarousel({
     loop: true,
     margin: 10,
     nav: false,
@@ -6,10 +6,31 @@ $('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayTimeout: 5000,
     autoplayHoverPause: true,
-    animateOut: 'fadeOut',
     responsive: {
         0: {
             items: 1
+        },
+    }
+})
+
+$('.owl-carousel-2').owlCarousel({
+    loop: false,
+    margin: 30,
+    nav: false,
+    dots: false,
+    dotsContainer: '.customDots',
+    autoplay: false,
+    responsive: {
+        769: {
+            items: 2,
+            dots: false,
+        },
+        320: {
+            dots: true,
+            dotsContainer: '.customDots',
+            dotClass: 'customDot',
+            items: 1,
+            autoWidth: true,
         },
     }
 })
