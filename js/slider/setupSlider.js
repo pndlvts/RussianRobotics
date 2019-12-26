2a58dcd37e4aa1209cd9400cec7fb5a226460b0b
Pandelov Timofey <pndlvts@gmail.com>
2019-12-26 15:25:01 +0300

    слайдер готов

---
+++ js/slider/setupSlider.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    animateOut: 'fadeOut',
+    responsive: {
+        0: {
+            items: 1
+        },
+    }
+})
