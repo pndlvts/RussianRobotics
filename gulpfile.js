f4514944d1f4b5f6a82c042931d3010fbf8968be
Pandelov Timofey <pndlvts@gmail.com>
2019-12-29 22:10:02 +0300

    конактинация в bundle js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
+var concat = require('gulp-concat');
 
 gulp.task("sass", function () {
     return gulp.src("sass/*.scss")
@@ -15,7 +16,14 @@ gulp.task("pug", function () {
     
 });
  
+gulp.task('concat', function() {
+  return gulp.src('blocks/**/*.js') // путь к папке со скриптами
+   .pipe(concat('bundle.js')) // в какой файл объединить
+   .pipe(gulp.dest('js/'));
+});
+
 gulp.task('watch', function () {
   gulp.watch('sass/*.scss', gulp.series('sass'));
   gulp.watch('pug/*.pug', gulp.series('pug'));
+  gulp.watch('blocks/**/*.js', gulp.series('concat'));
 });
---
+++ js/bundle.js
@@ -0,0 +1,138 @@
+$('.owl-carousel-1').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+    }
+})
+
+$('.owl-carousel-2').owlCarousel({
+    loop: false,
+    margin: 30,
+    nav: false,
+    dots: false,
+    dotsContainer: '.customDots',
+    autoplay: false,
+    responsive: {
+        1024: {
+            items: 2,
+            dots: false,
+        },
+        320: {
+            dots: true,
+            dotsContainer: '.customDots',
+            dotClass: 'customDot',
+            items: 1,
+            autoWidth: true,
+        },
+    }
+})
+
+var date = new Date;
+var todayDate = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+document.querySelector(".finance__date").innerHTML=todayDate;
+//гамбергер
+document.querySelector("#icon-menu").addEventListener("click", (e) => {
+    var mobileMenu = document.querySelector("ul.header__menu");
+    var menu = document.querySelector("div.header__menu");
+    if (document.body.clientWidth <= 767) {
+        mobileMenu.classList.toggle("header__menu_show");
+    } else {
+        menu.classList.toggle("header__menu_show");
+    }
+});
+//для частных лиц (на планшеты)
+document.querySelector("#for-person-tablet").addEventListener("click", (e) => {
+    var forPersonBlock = document.querySelector(".header__for-person-block");
+    forPersonBlock.classList.toggle("header__for-person-block_show");
+    document.querySelector("#for-person-tablet-link").classList.toggle("header__for-person_active");
+    var forCorpBlock = document.querySelector(".header__for-corp-block");
+    forCorpBlock.classList.remove("header__for-corp-block_show");
+    document.querySelector("#for-corp-tablet-link").classList.remove("header__for-corp_active");
+    var forPersonBlock = document.querySelector(".header__for-services-block");
+    forPersonBlock.classList.remove("header__for-services-block_show");
+    document.querySelector("#for-services-tablet-link").classList.remove("header__for-services_active");
+})
+//для частных лиц (на телефоны)
+document.querySelector("#for-person").addEventListener("click", (e) => {
+    var box = document.querySelectorAll(".header__for-person-list-item");
+    box.forEach(box => {
+        box.classList.toggle("header__for-person-list-item_show");
+    })
+    document.querySelector("#for-person-dropdown").classList.toggle("header__for-person_active");
+})
+//для юр лиц (на телефоны)
+document.querySelector("#for-corp").addEventListener("click", (e) => {
+    var box = document.querySelectorAll(".header__for-corp-list-item");
+    box.forEach(box => {
+        box.classList.toggle("header__for-corp-list-item_show");
+    })
+    document.querySelector("#for-corp-dropdown").classList.toggle("header__for-corp_active");
+})
+//для услуг на рынке ценных бумаг (на телефоны)
+document.querySelector("#for-services").addEventListener("click", (e) => {
+    var box = document.querySelectorAll(".header__for-services-list-item");
+    box.forEach(box => {
+        box.classList.toggle("header__for-services-list-item_show");
+    })
+    document.querySelector("#for-services-dropdown").classList.toggle("header__for-services_active");
+})
+//о банке (на телефоны)
+document.querySelector("#for-about-bank-mobile").addEventListener("click", (e) => {
+    var box = document.querySelectorAll(".header__for-about-bank-mobile-list-item");
+    box.forEach(box => {
+        box.classList.toggle("header__for-about-bank-mobile-list-item_show");
+    })
+    document.querySelector("#for-about-bank-mobile-dropdown").classList.toggle("header__for-about-bank-mobile_active");
+})
+//о банке (на планшеты)
+document.querySelector("#header__about-bank_tablet").addEventListener("click", (e) => {
+    if (document.body.clientWidth < 1024){
+    var box = document.querySelector("ul.header__about-bank-list");
+    box.classList.toggle("header__about-bank-list_show");
+    document.querySelector(".header__about-bank-drop").classList.toggle("header__about-bank-drop_active");
+    }
+})
+//верхний блок о банке для десктопов
+document.querySelector("#header__about-bank-button_desktop").addEventListener("click", (e) => {
+    var aboutBankHeaderBlock = document.querySelector(".header__about-bank-block_desktop");
+    aboutBankHeaderBlock.classList.toggle("header__about-bank-block_display");
+});
+//закрыть верхний блок о банке для десктопов
+document.querySelector("#header__about-bank-button-close_desktop").addEventListener("click", (e) => {
+    var aboutBankHeaderBlock = document.querySelector(".header__about-bank-block_desktop");
+    aboutBankHeaderBlock.classList.remove("header__about-bank-block_display");
+});
+//
+//для юр лиц (на планшеты)
+document.querySelector("#for-corp-tablet").addEventListener("click", (e) => {
+    var forCorpBlock = document.querySelector(".header__for-corp-block");
+    forCorpBlock.classList.toggle("header__for-corp-block_show");
+    document.querySelector("#for-corp-tablet-link").classList.toggle("header__for-corp_active");
+    var forPersonBlock = document.querySelector(".header__for-person-block");
+    forPersonBlock.classList.remove("header__for-person-block_show");
+    document.querySelector("#for-person-tablet-link").classList.remove("header__for-person_active");
+    var forPersonBlock = document.querySelector(".header__for-services-block");
+    forPersonBlock.classList.remove("header__for-services-block_show");
+    document.querySelector("#for-services-tablet-link").classList.remove("header__for-services_active");
+})
+//для услуг (на планшеты)
+document.querySelector("#for-services-tablet").addEventListener("click", (e) => {
+    var forCorpBlock = document.querySelector(".header__for-services-block");
+    forCorpBlock.classList.toggle("header__for-services-block_show");
+    document.querySelector("#for-services-tablet-link").classList.toggle("header__for-services_active");
+    var forPersonBlock = document.querySelector(".header__for-person-block");
+    forPersonBlock.classList.remove("header__for-person-block_show");
+    document.querySelector("#for-person-tablet-link").classList.remove("header__for-person_active");
+    var forCorpBlock = document.querySelector(".header__for-corp-block");
+    forCorpBlock.classList.remove("header__for-corp-block_show");
+    document.querySelector("#for-corp-tablet-link").classList.remove("header__for-corp_active");
+})
+
