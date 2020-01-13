0571c8f5cea88eeb8b014d5e13d6a8a6071c43b4
Pandelov Timofey <pndlvts@gmail.com>
2020-01-13 14:52:59 +0300

    верстка готова, осталось подровнять

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -2,3 +2,15 @@ document.querySelector(".footer__list-item-dropdown-1").addEventListener("click"
     document.querySelector(".footer__list-item-dropdown-1").classList.toggle("after_active")
     document.querySelector(".footer__dropdown-list-1").classList.toggle("footer__dropdown-list_show")
 })
+document.querySelector(".footer__list-item-dropdown-2").addEventListener("click", function(){
+    document.querySelector(".footer__list-item-dropdown-2").classList.toggle("after_active")
+    document.querySelector(".footer__dropdown-list-2").classList.toggle("footer__dropdown-list_show")
+})
+document.querySelector(".footer__list-item-dropdown-3").addEventListener("click", function(){
+    document.querySelector(".footer__list-item-dropdown-3").classList.toggle("after_active")
+    document.querySelector(".footer__dropdown-list-3").classList.toggle("footer__dropdown-list_show")
+})
+document.querySelector(".footer__list-item-dropdown-4").addEventListener("click", function(){
+    document.querySelector(".footer__list-item-dropdown-4").classList.toggle("after_active")
+    document.querySelector(".footer__dropdown-list-4").classList.toggle("footer__dropdown-list_show")
+})
