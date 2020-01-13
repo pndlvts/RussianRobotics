903c20af712e78bc8288f33059219212110950b8
Pandelov Timofey <pndlvts@gmail.com>
2020-01-13 13:56:59 +0300

    мобилка почти готова

---
+++ blocks/footer/footer.js
@@ -0,0 +1,4 @@
+document.querySelector(".footer__list-item-dropdown-1").addEventListener("click", function(){
+    document.querySelector(".footer__list-item-dropdown-1").classList.toggle("after_active")
+    document.querySelector(".footer__dropdown-list-1").classList.toggle("footer__dropdown-list_show")
+})
